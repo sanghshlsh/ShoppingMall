31492ef6bda01729acb308408db074b7719e2176
hyunjeep <hyunjeep@gamil.com>
2020-07-22 12:28:02 +0900

    git 체험중

--- src/main/java/kr/co/domain/ProductDTO.java
+++ src/main/java/kr/co/domain/ProductDTO.java
@@ -2,11 +2,8 @@ package kr.co.domain;
 
 import java.io.Serializable;
 
-public class ProductDTO implements Serializable{
+public class ProductDTO implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 		
