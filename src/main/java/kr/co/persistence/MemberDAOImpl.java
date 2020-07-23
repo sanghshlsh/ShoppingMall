61bf14de2fd4da0ae7d8e758876da90e1f264d24
sanghshlsh <sanghshlsh@gmail.com>
2020-07-24 08:55:51 +0900

    manager-product-list대략

--- src/main/java/kr/co/persistence/MemberDAOImpl.java
+++ src/main/java/kr/co/persistence/MemberDAOImpl.java
@@ -2,7 +2,9 @@ package kr.co.persistence;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemberDAOImpl implements MemberDAO {
 	
 	@Autowired
--- src/main/java/kr/co/service/MemberServiceImpl.java
+++ src/main/java/kr/co/service/MemberServiceImpl.java
@@ -1,9 +1,11 @@
 package kr.co.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kr.co.persistence.MemberDAO;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 	
 	@Autowired
