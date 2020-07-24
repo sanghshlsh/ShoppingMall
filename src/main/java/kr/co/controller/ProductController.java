820ba32be6b40de4f0d1d0be0e72f4565be802ab
hyunjeep <hyunjeep@gamil.com>
2020-07-24 11:07:05 +0900

    admin manage 생성

--- src/main/java/kr/co/controller/ProductController.java
+++ src/main/java/kr/co/controller/ProductController.java
@@ -7,10 +7,5 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ProductController {
 
-	@RequestMapping(value="/admin/product/list", method = RequestMethod.GET)
-	public void adminProduct() {		
-	}
-	@RequestMapping(value="/admin/index", method = RequestMethod.GET)
-	public void adminIndex() {		
-	}
+
 }
