99cfd2d00a6e5da37ccf753687dc0c533bf109a8
znlsdlwm <zldtm6@gmail.com>
2020-07-24 10:21:07 +0900

    m

--- src/main/java/kr/co/controller/MemberController.java
+++ src/main/java/kr/co/controller/MemberController.java
@@ -3,8 +3,10 @@ package kr.co.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import kr.co.domain.MemberDTO;
 import kr.co.service.MemberService;
 
 @Controller
@@ -14,5 +16,6 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
+	
 
 }
