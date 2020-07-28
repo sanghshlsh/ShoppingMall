f73ecbc8bbed873f50c6805ba82076d8b3b1c81f
hyunjeep <hyunjeep@gamil.com>
2020-07-28 09:18:14 +0900

    수정없음

--- src/main/java/kr/co/controller/ProductController.java
+++ src/main/java/kr/co/controller/ProductController.java
@@ -1,56 +1,15 @@
 package kr.co.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("product")
 public class ProductController {
-
-<<<<<<< HEAD
-=======
-
-//	@RequestMapping(value="/admin/product/list", method = RequestMethod.GET)
-//	public void adminProduct() {		
-//	}
-	@RequestMapping(value="/admin/default/index", method = RequestMethod.GET)
-	public void adminIndex() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/blank", method = RequestMethod.GET)
-	public void adminBlank() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/buttons", method = RequestMethod.GET)
-	public void adminButtons() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/cards", method = RequestMethod.GET)
-	public void adminCards() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/tables", method = RequestMethod.GET)
-	public void adminTables() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/charts", method = RequestMethod.GET)
-	public void adminCharts() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/animation", method = RequestMethod.GET)
-	public void adminAniamtion() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/boarder", method = RequestMethod.GET)
-	public void adminBoarder() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/color", method = RequestMethod.GET)
-	public void adminColor() {
-		//필요x
-	}
-	@RequestMapping(value="/admin/default/other", method = RequestMethod.GET)
-	public void adminOther() {
-		//필요x
+	@RequestMapping(value = "/product", method = RequestMethod.GET)
+	public void productIndex() {
 	}
 
->>>>>>> fetch_head
+
 }
