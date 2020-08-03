a2e600093d58d113176d3c5993ca67ba03cf0825
sanghshlsh <sanghshlsh@gmail.com>
2020-08-03 15:55:28 +0900

    conflict 수정

--- src/main/java/kr/co/controller/ProductController.java
+++ src/main/java/kr/co/controller/ProductController.java
@@ -4,30 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-<<<<<<< HEAD
-import kr.co.domain.ProductDTO;
-import kr.co.service.ProductService;
-
-=======
 
 import kr.co.domain.ProductDTO;
 import kr.co.service.ProductService;
 
-
-
->>>>>>> fetch_head
 @Controller
 public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 
-<<<<<<< HEAD
+
 	@RequestMapping(value = "/admin/product/insert", method = RequestMethod.GET)
 	public String insertProduct(ProductDTO pdto) {
 		productService.insert(pdto);
 		return "redirct:/admin/insert";
-=======
+	}
 
 
 	@RequestMapping(value = "/product", method = RequestMethod.GET)
@@ -80,7 +72,6 @@ public class ProductController {
 	@RequestMapping(value="/admin/main", method = RequestMethod.GET)
 	public void adminMain() {
 		//필요x
->>>>>>> fetch_head
 	}
 
 }
