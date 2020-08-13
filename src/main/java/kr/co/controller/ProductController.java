59c1d8a74aa0473da090e86ea258f50f8ab17457
sanghshlsh <sanghshlsh@gmail.com>
2020-08-13 17:22:39 +0900

    메인화면로그인링크연결

--- src/main/java/kr/co/controller/ProductController.java
+++ src/main/java/kr/co/controller/ProductController.java
@@ -1,41 +1,12 @@
 package kr.co.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-<<<<<<< HEAD
-
-import kr.co.service.ProductService;
-=======
-import kr.co.service.ProductService;
-
->>>>>>> fetch_head
 
 @Controller
 public class ProductController {
 
-	@Autowired
-	private ProductService productService;
 
-<<<<<<< HEAD
-=======
-
-	/*
-	 * @RequestMapping(value = "/admin/product/insert", method = RequestMethod.GET)
-	 * public String insertProduct(ProductDTO pdto) { productService.insert(pdto);
-	 * return "redirct:/admin/insert"; }
-	 */
->>>>>>> fetch_head
-
-	@RequestMapping(value = "/product", method = RequestMethod.GET)
-	public void productIndex() {
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> fetch_head
-	}
+	
 }
